240195d7387826799815a1a04f6aa13c8ec7083c
tdeveth <thijsdeveth@gmail.com>
2018-01-14 23:08:02 +0100

    Triller class no longer needed in demo

--- js/triller.js
+++ js/triller.js
@@ -1,21 +1,22 @@
 
 var Triller = function () {
 
-    var duration;
-    var element;
     /**
      * Initializes the module
-     * @param {string} [selector] Css selector that targets the element that needs to bounce
+     * @param {string} [selector] Css selector that targets the element that needs to shake
      * @param {string} [duration] Duration in seconds of one shake
      */
-    var init = function (selector, duration) {
-        duration = duration;
+    var start = function (selector, duration) {
         element = document.querySelector(selector);
+        element.style.animationName = 'tril';
         element.style.animationDuration = duration;
+        element.style.animationTimingFunction = 'cubic-bezier(.36,.07,.19,.97)';
+        element.style.animationIterationCount = 'infinite';
+        element.style.transform = 'translateX(0)';
     };
 
     //Return the functions that should be accessible from the outside. The rest is only accessible from within the object
     return {
-        init: init
+        start: start
     };
 };
